e53506c61519169d69cf3bf48197c18a06d66fa8
gztong <gztong@cpp.edu>
2017-01-31 10:50:38 -0800

    Added Comments

--- Algorithm1.java
+++ Algorithm1.java
@@ -15,9 +15,6 @@ import java.util.Set;
  */
 public class Algorithm1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int[] test = generateArr(1000000);
         long startTime = System.currentTimeMillis();
@@ -33,18 +30,26 @@ public class Algorithm1 {
         int start = 0;
         int end = set.length - 1;
         while (start < end) {
+            // Partitions into S!, S2, and S3
             int pivot = partition(set, start, end);
-            if (pivot < k) {
-                start = pivot + 1;
-            } else if (pivot > k) {
+            //if S1.length > k, then partition the left side
+            if (pivot > k) {
                 end = pivot - 1;
+            } 
+            // if S1.length < k, then partition the right side
+            else if (pivot < k) {
+                start = pivot + 1;
             } else {
+                // S1.length = k, so return the number
                 return set[pivot];
             }
         }
         return set[start];
     }
-
+    
+    //Partitions the array into two separate arrays
+    //Elements that are less than the pivot are on the left
+    //Elements that are greatere than the pivot are on the right
     private static int partition(int[] set, int start, int end) {
         int pivot = start, temp;
         while (start <= end) {
@@ -71,7 +76,8 @@ public class Algorithm1 {
         set[pivot] = temp;
         return end;
     }
-
+    
+    //Generates the array used for testing
     private static int[] generateArr(int len) {
         Set<Integer> random = new HashSet<Integer>();
         for (int i = 0; i < len; i++) {
